39acbc8eb5e27d39583ab97a54668ef56a365a0b
Hachapuri <karailymit@gmail.com>
2025-01-06 17:26:21 +0500

    Add files via upload

---
+++ lesson5/main.py
@@ -0,0 +1,92 @@
+import os
+import file_operations as fops
+import random
+from faker import Faker
+
+
+fake = Faker("ru_RU")
+
+runic_alphabet = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+skills = [
+    "Стремительный прыжок",
+    "Электрический выстрел",
+    "Ледяной удар",
+    "Стремительный удар",
+    "Кислотный взгляд",
+    "Тайный побег",
+    "Ледяной выстрел",
+    "Огненный заряд"
+]
+
+
+def create_character(num_of_characters):
+    for character_num in range(num_of_characters):
+        if random.randint(0, 1):
+            first_name = fake.first_name_male()
+            last_name = fake.last_name_male()
+        else:
+            first_name = fake.first_name_female()
+            last_name = fake.last_name_female()
+
+        job = fake.job()
+        town = fake.city()
+
+        non_repetitive_skills = random.sample(skills, 3)
+        runic_skills = []
+
+        for skill in non_repetitive_skills:
+            runic_skill = ""
+            for letter in skill:
+                runic_skill += runic_alphabet[letter]
+            runic_skills.append(runic_skill)
+
+        context = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "job": job,
+            "town": town,
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": runic_skills[0],
+            "skill_2": runic_skills[1],
+            "skill_3": runic_skills[2]
+        }
+        output_path = os.path.join("output", "svg", f"person-{character_num+1}.svg")
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        fops.render_template("charsheet.svg", output_path, context)
+
+
+def main():
+    create_character(10)
+
+
+if __name__ == '__main__':
+    main()
